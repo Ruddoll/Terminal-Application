782a6f4871c5dc7acfa1a9ed97f526279b19adab
Ruddoll <moyo.adewusi@gmail.com>
2023-02-02 14:37:40 +1100

    got started on coloring outputs

--- Directory Manager.py
+++ Directory Manager.py
@@ -1,6 +1,16 @@
 import os
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def display_files(directory):
     try:
         return os.listdir(directory)

affd457b8d2df9bd78af88a03557de2a26007a96
Ruddoll <moyo.adewusi@gmail.com>
2023-02-02 14:55:30 +1100

    added feature; can now rename directory

--- Directory Manager.py
+++ Directory Manager.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import shutil
 
 def display_files(directory):
     try:
@@ -26,12 +25,12 @@ def delete_directory(directory):
     except:
         return "An error occurred while deleting the directory"
 
-def move_directory(src, dst):
+def rename_directory(old_name, new_name):
     try:
-        shutil.move(src, dst)
-        return "Directory moved successfully"
+        os.rename(old_name, new_name)
+        return "Directory renamed successfully"
     except:
-        return "An error occurred while moving the directory"
+        return "An error occurred while renaming the directory"
 
 def search_directories(name, path):
     directories = []
@@ -47,7 +46,7 @@ def main_menu():
     print("1. Display files in a directory")
     print("2. Make a directory")
     print("3. Delete a directory")
-    print("4. Move a directory")
+    print("4. Rename a directory")
     print("5. Search for a directory")
     print("6. Quit")
 
@@ -63,9 +62,9 @@ def handle_choice(choice):
         directory = input("Enter the name of the directory: ")
         print(delete_directory(directory))
     elif choice == '4':
-        src = input("Enter the source path of the directory: ")
-        dst = input("Enter the destination path for the directory: ")
-        print(move_directory(src, dst))
+        old_name = input("Enter the current name of the directory: ")
+        new_name = input("Enter the new name of the directory: ")
+        print(rename_directory(old_name, new_name))
     elif choice == '5':
         name = input("Enter the name of the directory: ")
         path = input("Enter the starting path for the search: ")

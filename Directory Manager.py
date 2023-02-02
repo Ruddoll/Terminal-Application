f2992b6d5073aa2e98085dd385b035d22c9f263a
Ruddoll <moyo.adewusi@gmail.com>
2023-02-02 13:55:02 +1100

    can now make and delete directories

--- Directory Manager.py
+++ Directory Manager.py
@@ -1,34 +1,51 @@
-import sys
 import os
 
 def display_files(directory):
     try:
-        files = os.listdir(directory)
-        for file in files:
-            print(file)
+        return os.listdir(directory)
     except:
-        print("An error occurred while accessing the directory")
-        sys.exit()
+        return "An error occurred while accessing the directory"
+
+def make_directory(directory):
+    try:
+        os.mkdir(directory)
+        return "Directory created successfully"
+    except:
+        return "An error occurred while creating the directory"
+
+def delete_directory(directory):
+    try:
+        os.rmdir(directory)
+        return "Directory deleted successfully"
+    except:
+        return "An error occurred while deleting the directory"
 
 def main_menu():
     print("Directory Manager")
     print("========================")
     print("1. Display files in a directory")
-    print("2. Quit")
+    print("2. Make a directory")
+    print("3. Delete a directory")
+    print("4. Quit")
 
 def handle_choice(choice):
     if choice == '1':
-        directory = input("Enter the directory path: ")
-        display_files(directory)
+        directory = input("Enter the path of the directory: ")
+        print(display_files(directory))
     elif choice == '2':
+        directory = input("Enter the name of the directory: ")
+        print(make_directory(directory))
+    elif choice == '3':
+        directory = input("Enter the name of the directory: ")
+        print(delete_directory(directory))
+    elif choice == '4':
+        print("Exiting the program")
         sys.exit()
     else:
         print("Invalid choice. Try again.")
 
-def main():
-    main_menu()
-    choice = input("Enter your choice (1 or 2): ")
-    handle_choice(choice)
-
 if __name__ == '__main__':
-    main()
+    while True:
+        main_menu()
+        choice = input("Enter your choice: ")
+        handle_choice(choice)
